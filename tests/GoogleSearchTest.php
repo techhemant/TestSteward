dedd455e86ce34b7f5d266cb77bce1e141882b50
techhemant <hemant.ojha@bigcommerce.com>
2018-10-06 13:09:12 +1000

    first commit

---
+++ tests/GoogleSearchTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace My;
+
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Lmc\Steward\Test\AbstractTestCase;
+
+/**
+ * Created by PhpStorm.
+ * User: hemant.ojha
+ * Date: 6/10/18
+ * Time: 12:04 PM
+ */
+class GoogleSearchTest extends AbstractTestCase
+{
+    /**
+     *
+     */
+    public function testGoogleSearchResults()
+    {
+
+        try {
+            $this->wd->get('http://www.google.com');
+            $this->assertContains("Google", $this->wd->getTitle());
+
+            $this->log('Current page "%s" has title "%s"', $this->wd->getCurrentURL(), $this->wd->getTitle());
+
+
+            $this->findByCss("input[name='q']")->sendKeys("Selenium");
+
+            $this->findByCss("input[value='Google Search']")->click();
+
+
+            $this->log($this->findByCss(".srg .r>a")->getText());
+
+        } catch (NoSuchElementException $e) {
+
+            echo $e->getResults();
+        }
+
+
+    }
+    /**
+     *
+     */
+    public function testGoogleSearchResults1()
+    {
+
+        try {
+            $this->wd->get('http://www.google.com');
+            $this->assertContains("Google", $this->wd->getTitle());
+
+            $this->log('Current page "%s" has title "%s"', $this->wd->getCurrentURL(), $this->wd->getTitle());
+
+
+            $this->findByCss("input[name='q']")->sendKeys("Selenium");
+
+            $this->findByCss("input[value='Google Search']")->click();
+
+
+            $this->log($this->findByCss(".srg .r>a")->getText());
+
+        } catch (NoSuchElementException $e) {
+
+            echo $e->getResults();
+        }
+
+
+    }
+    /**
+     *
+     */
+    public function testGoogleSearchResults2()
+    {
+
+        try {
+            $this->wd->get('http://www.google.com');
+            $this->assertContains("Google", $this->wd->getTitle());
+
+            $this->log('Current page "%s" has title "%s"', $this->wd->getCurrentURL(), $this->wd->getTitle());
+
+
+            $this->findByCss("input[name='q']")->sendKeys("Selenium");
+
+            $this->findByCss("input[value='Google Search']")->click();
+
+
+            $this->log($this->findByCss(".srg .r>a")->getText());
+
+        } catch (NoSuchElementException $e) {
+
+            echo $e->getResults();
+        }
+
+
+    }
+    /**
+     *
+     */
+    public function testGoogleSearchResults3()
+    {
+
+        try {
+            $this->wd->get('http://www.google.com');
+            $this->assertContains("Google", $this->wd->getTitle());
+
+            $this->log('Current page "%s" has title "%s"', $this->wd->getCurrentURL(), $this->wd->getTitle());
+
+
+            $this->findByCss("input[name='q']")->sendKeys("Selenium");
+
+            $this->findByCss("input[value='Google Search']")->click();
+
+
+            $this->log($this->findByCss(".srg .r>a")->getText());
+
+        } catch (NoSuchElementException $e) {
+
+            echo $e->getResults();
+        }
+
+
+    }
+
+}
---
+++ vendor/lmc/steward/src/Component/LegacyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lmc\Steward\Component;
+
+use Exception;
+
+/**
+ * Exception raised by Legacy
+ */
+class LegacyException extends \Exception
+{
+}
